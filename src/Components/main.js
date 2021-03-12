b35350d5cd886dfedf157780c7b9be0da719b9cc
A Joseph <alecrewinkel@gmail.com>
2021-03-12 16:08:06 -0700

    first page

--- src/Components/main.js
+++ src/Components/main.js
@@ -9,7 +9,6 @@ import Wishlist from './wishlist';
 const Main = () => (
     <Switch>
         <Route exact path="/" component={LandingPage}/>
-        <Route path="/landingpage" component={LandingPage}/>
         <Route path="/wishlist" component={Wishlist}/>
         <Route path="/myalbums" component={MyAlbums}/>
         <Route path="/mysongs" component={MySongs}/>
